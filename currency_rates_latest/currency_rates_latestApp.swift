155e3df6da11743a72885ad63fc5d049f760e002
asad-co <realhk98000@gmail.com>
2024-11-02 16:22:34 +0500

    Initial Commit

---
+++ currency_rates_latest/currency_rates_latestApp.swift
@@ -0,0 +1,17 @@
+//
+//  currency_rates_latestApp.swift
+//  currency_rates_latest
+//
+//  Created by Kstom Glasses on 02/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct currency_rates_latestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
